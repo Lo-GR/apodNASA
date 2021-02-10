aa5fb0d5ad27c499e0f3ffccaa08b7cebfd7f9ea
Lo-GR <diamondintheroth@gmail.com>
2021-02-10 11:38:01 -0800

    create logic, refactor, and amend styling
    Co-authored-by: Ash Porter <porter.ashley13@gmail.com>

---
+++ src/apod.js
@@ -0,0 +1,17 @@
+export default class ApodReturn{
+  static searchAPOD (date){
+    return new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      const api = `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}&date=${date}`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", api, true);
+      request.send();
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -4,7 +4,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ApodReturn from './apod.js';
 
 $(document).ready(function(){
-
+  $("form#dateSearch").submit(function(event) {
+    event.preventDefault();
+    const dateSearch = $("#dateInput").val();
+    console.log(dateSearch);
+    let promise = ApodReturn.searchAPOD(dateSearch);
+    promise.then(function (response) {
+      const body = JSON.parse(response);
+      $('#resultDate').text(`${body.date}`);
+      $('#resultImg').html(`<img src="${body.url}" alt="space image">`);
+      $('#resultDesc').text(body.explanation);
+      // $('#results').text(`${body.date}`);
+    }, function (error) {
+      $('#results').text(`There was an error processing your request: ${error}`);
+    });
+  });
 });
